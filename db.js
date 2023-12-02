992c6bf68cb3325520ca3b7cdde7e1b81effd694
Carmel Meelind <cmeelind@tktk.ee>
2023-12-02 15:43:03 +0200

    week 11 dotenv

--- db.js
+++ db.js
@@ -1,10 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const { Pool } = require('pg');
-const itemsPool = new Pool({
-    user: 'postgres',
-    password: 'Serverid!1556',
-    host: 'localhost',
-    port: 5432,
-    database: 'myrecipes'
+
+const itemsPool = new Pool ({
+    connectionString: process.env.DBConnectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
+// const itemsPool = new Pool({
+//     user: 'postgres',
+//     password: 'Serverid!1556',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'myrecipes'
+// });
 
 module.exports = itemsPool;
